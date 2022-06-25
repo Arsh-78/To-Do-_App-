97f3861ccfaa355ea7ecde99e2634537f20942f6
Arsh-78 <arshbz53@gmail.com>
2022-06-25 19:04:04 +0530

    Used Global State and passing callbacks for first time See,Comments in tasktile file

---
+++ lib/screens/add_task_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class AddTaskScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color(0xff757575),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20.0),
+            topRight: Radius.circular(20.0),
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 30.0, left: 20.0, right: 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                'Add Task',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.green, fontSize: 30.0),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              TextField(autofocus: true, textAlign: TextAlign.center),
+              TextButton(
+                onPressed: () {},
+                child: Text(
+                  'Add',
+                  style: TextStyle(color: Colors.white),
+                ),
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.green)),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/tasks_screen.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/task_list.dart';
+import 'add_task_screen.dart';
+
+class TaskScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF22B07E),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          showModalBottomSheet(
+            isScrollControlled: true,
+            context: context,
+            builder: (context) => SingleChildScrollView(
+              padding: EdgeInsets.only(
+                  bottom: MediaQuery.of(context).viewInsets.bottom),
+              child: AddTaskScreen(),
+            ),
+          );
+        },
+        backgroundColor: Color(0xFF22B07E),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.only(
+                top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 30.0,
+                  child: Icon(Icons.list, color: Color(0xFF22B07E), size: 30.0),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  'ToDoey',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.w700),
+                ),
+                Text(
+                  '12 tasks',
+                  style: TextStyle(color: Colors.white, fontSize: 15.0),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              height: 300.0,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(20.0),
+                  topLeft: Radius.circular(20.0),
+                ),
+              ),
+              child: TasksList(),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
